73a5c36688aaf23620075140de0cb2db54e52052
Khalid LAABALI <khalidlaabali@gmail.com>
2023-03-22 05:19:30 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Adds two values
+ * @n1: The first number to be cumpted
+ * @n2: The seconde number to be cumpted
+ * Return: Integer value
+ */
+
+int add(int n1, int n2)
+
+{
+	return (n1 + n2);
+
+}
