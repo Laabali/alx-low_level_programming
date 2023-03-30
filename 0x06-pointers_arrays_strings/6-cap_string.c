df9cc026fd3a3de77d7983fd0e0671da1e92e797
Khalid LAABALI <khalidlaabali@gmail.com>
2023-03-30 15:19:51 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,31 +2,29 @@
 
 /**
  * cap_string - capitalizes all words in a string
- * @s: string
+ * @s: string to modify
  *
- * Return: adress of s
+ * Return: the resulting string
  */
 char *cap_string(char *s)
 {
-	int i = 0, j;
-	char a[] = "\t\n,;.!?\"(){}";
+	int i, j;
+	char spe[13] = {' ', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 
-	while (*(s + i))
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+		for (j = 0; j < 13; j++)
 		{
-			if (i == 0)
-				*(s + i) -= 'a' - 'A';
-			else
+			if (s[i] == spe[j])
 			{
-				for (j = 0; j <= 12; j++)
-				{
-					if (a[j] == *(s + i - 1))
-						*(s + i) -= 'a' - 'A';
-				}
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			{
+				s[i + 1] -= 32;
+			}
 			}
 		}
-		i++;
 	}
 	return (s);
 }
