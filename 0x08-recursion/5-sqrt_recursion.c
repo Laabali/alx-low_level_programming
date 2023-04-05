083ab74e0581d9c33929c1707b4056e6110e7d27
Khalid LAABALI <khalidlaabali@gmail.com>
2023-04-05 16:07:18 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -15,8 +15,9 @@ int check(int a, int b)
 		return (-1);
 	return (check(a + 1, b));
 }
+
 /**
- * _sqrt_recusion - returns the natural square root of a number
+ * _sqrt_recursion - returns the natural square root of a number
  * @n: integer to find sqrt of
  * Return: natural square root or -1
  */
