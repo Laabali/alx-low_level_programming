c81f4d67ccee90084650748c598321da08bf2091
Khalid LAABALI <khalidlaabali@gmail.com>
2023-04-19 16:43:45 -0700

    2-int_index.c 3-main.c 3-op_functions.c 3-get_op_func.c 3-calc.h

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,7 +7,7 @@
  * @action: pointer to print in regular or hex
  * Return: void
  */
-void arrat_iterator(int *array, size_t size, void (*action)(int))
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
