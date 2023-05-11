cc9db0dc64c6e6407c82d67a5e7bbe7aa21cd5a7
laabali <khalidlaabali@gmail.com>
2023-05-11 03:09:33 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,7 +6,7 @@
  * @text_content: A pointer to a string to write to the file.
  *
  * Return: If the function fails - -1.
- *	Otherwise - 1.
+ *         Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (filename != NULL)
+	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
 			len++;
@@ -23,7 +23,6 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, len);
-
 	if (fd == -1 || w == -1)
 		return (-1);
 
